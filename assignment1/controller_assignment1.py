dbd4b3d760e0e2d966c037cb98a2d69d6524406f
paarth-sharma <paarthsharma2003@gmail.com>
2025-02-12 00:19:20 +0000

    fixed implementation of assignment1 rules and tcp handling

--- assignment1/controller_assignment1.py
+++ assignment1/controller_assignment1.py
@@ -15,14 +15,26 @@ table={}
 
 rules=[
     #QoS Rules
-    {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:03', 'TCPPort':40, 'queue':0, 'drop':False},# H1->H3:30Mbps (queue 0 on eth3)
-    {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:02', 'TCPPort':60, 'queue':1, 'drop':False},# H1->H2:150Mbps (queue 1 on eth2)
-    {'EthSrc':'00:00:00:00:00:02', 'EthDst':'00:00:00:00:00:04','queue':1, 'drop':False}, # H2->H4:200Mbps (queue 1 on eth4)
-    # => the first two example of rules have been added for you, you need now to add other rules to satisfy the assignment requirements. Notice that we will make decisions based on Ethernet address rather than IP address. Rate limiting is implemented by sending the pacet to the correct port and queue (the queues that you have specified in the topology file).
-    
+    # => the first two example of rules have been added for you, you need now to add other rules to satisfy the assignment requirements. 
+    # Notice that we will make decisions based on Ethernet address rather than IP address. Rate limiting is implemented by sending the pacet 
+    # to the correct port and queue (the queues that you have specified in the topology file).
+    {'EthSrc':'00:00:00:00:00:01', 'EthDst':'00:00:00:00:00:03', 'TCPPort':40, 'queue':0, 'drop':False},  # H1->H3:30Mbps (queue 0 on eth3)
+    {'EthSrc':'00:00:00:00:00:01', 'EthDst':'00:00:00:00:00:02', 'TCPPort':60, 'queue':1, 'drop':False},  # H1->H2:150Mbps (queue 1 on eth2)
+    {'EthSrc':'00:00:00:00:00:01', 'EthDst':'00:00:00:00:00:04', 'TCPPort':-1, 'queue':-1, 'drop':False}, # H1->H4:uncapped
+
+    {'EthSrc':'00:00:00:00:00:02', 'EthDst':'00:00:00:00:00:04', 'TCPPort':-1, 'queue':1, 'drop':False},  #H2->H4:200Mbps (queue 1 on eth4)
+    {'EthSrc':'00:00:00:00:00:02', 'EthDst':'00:00:00:00:00:01', 'TCPPort':-1, 'queue':-1, 'drop':False}, #H2->H1:uncapped
+    {'EthSrc':'00:00:00:00:00:02', 'EthDst':'00:00:00:00:00:03', 'TCPPort':-1, 'queue':-1, 'drop':False}, #H2->H3:uncapped
+
+    {'EthSrc':'00:00:00:00:00:03', 'EthDst':'00:00:00:00:00:01', 'TCPPort':-1, 'queue':-1, 'drop':False}, #H3->H1:uncapped
+    {'EthSrc':'00:00:00:00:00:03', 'EthDst':'00:00:00:00:00:02', 'TCPPort':-1, 'queue':-1, 'drop':False}, #H3->H2:uncapped
+
+    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:01', 'TCPPort':-1, 'queue':-1, 'drop':False}, #H4->H1:uncapped
+    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:02', 'TCPPort':-1, 'queue':-1, 'drop':False}, #H4->H2:uncapped
+
     # Firewall Rules
-    {'EthSrc':'00:00:00:00:00:03', 'EthDst':'00:00:00:00:00:04', 'drop':True},  # Block H3<->H4
-    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:03', 'drop':True}
+    {'EthSrc':'00:00:00:00:00:03', 'EthDst':'00:00:00:00:00:04', 'TCPPort':-1, 'queue':-1, 'drop':True},  # Block H3<->H4
+    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:03', 'TCPPort':-1, 'queue':-1, 'drop':True}
     
 ]
 
@@ -56,63 +68,72 @@ def _handle_PacketIn ( event): # Ths is the main class where your code goes, it 
 
     if dst_port is None and eth_packet.type == eth.ARP_TYPE and eth_packet.dst == EthAddr(b"\xff\xff\xff\xff\xff\xff"): # this identifies that the packet is an ARP broadcast
         # => in this case you want to create a packet so that you can send the message as a broadcast
-         # Create ARP flood packet
+        # Create ARP flood packet
         msg = of.ofp_packet_out(data=event.ofp)
         msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
         event.connection.send(msg)
-        return  # Exit after handling ARP broadcast
 
-    for rule in rules: #now you are adding rules to the flow tables like before. First you check whether there is a rule match based on Eth source and destination
-        if (eth_packet.dst==EthAddr(rule['EthDst']) and eth_packet.src==EthAddr(rule['EthSrc'])):
-            log.debug("Event: found rule from source %s to dest  %s" % (eth_packet.src, eth_packet.dst))
-            # => start creating a new flow rule for mathcing the ethernet source and destination
-            
-            # Create flow mod
-            fm = of.ofp_flow_mod()
-            fm.match = of.ofp_match.from_packet(eth_packet, inport)
-            fm.hard_timeout = 40
-            
-            #if ...
-            # => now check if the rule contains also TCP port info. If not install the flow without any port restriction
-                # => also remember to check if this is a drop rule. The drop function can be added by not sending any action to the flow rule
-                # => also remember that if there is a QoS requirement, then you need to use the of.ofp_action_enqueue() function, instead of the of.ofp_action_output
-                # => and remember that in addition to creating a fow rule, you should also send out the message that came from the Switch
-                # => at the end remember to send out both flow rule and packet
+    else:
+        for rule in rules: #now you are adding rules to the flow tables like before. First you check whether there is a rule match based on Eth source and destination
+            if (eth_packet.dst==EthAddr(rule['EthDst']) and eth_packet.src==EthAddr(rule['EthSrc'])):
+                log.debug("Event: found rule from source %s to dest  %s" % (eth_packet.src, eth_packet.dst))
+                # => start creating a new flow rule for mathcing the ethernet source and destination
+                
+                # Create flow mod
+                fm = of.ofp_flow_mod()
+                fm.match = of.ofp_match.from_packet(eth_packet, inport)
+                fm.hard_timeout = 40
+                #if ...
+                # => now check if the rule contains also TCP port info. If not install the flow without any port restriction
+                    # => also remember to check if this is a drop rule. The drop function can be added by not sending any action to the flow rule
+                    # => also remember that if there is a QoS requirement, then you need to use the of.ofp_action_enqueue() function, instead of the of.ofp_action_output
+                    # => and remember that in addition to creating a fow rule, you should also send out the message that came from the Switch
+                    # => at the end remember to send out both flow rule and packet
+                
+                # TCP port handling
+                if rule['TCPPort'] != -1:
+                    if not isinstance(eth_packet.next, pkt.ipv4):
+                        continue
+                    if not isinstance(eth_packet.next.next, pkt.tcp):
+                        continue
+                    if eth_packet.next.next.dstport != rule['TCPPort']:
+                        continue
+                    
+                    fm.match.dl_type = eth.IP_TYPE
+                    fm.match.nw_proto = pkt.ipv4.TCP_PROTOCOL
+                    fm.match.tp_dst = rule['TCPPort']
 
-            #else ...
-            # => otherwise:
-            # => if the packet is an IP packet, its protocol is TCP, and the TCP port of the packet matches the TCP rule above
-                # => add additioinal matching fileds to the flow rule you are creating: IP-protocol type, TCP_protocol_type, destination TCP port.
-                # => like above if it requires QoS then use the of.ofp_action_enqueue() function
-                # => also remember to check if this is a drop rule.
-                # => at the end remember to send out both flow rule and packet
-
-             # TCP port check
-            if 'TCPPort' in rule:
-                if not (isinstance(eth_packet.next, pkt.ipv4) or 
-                       not isinstance(eth_packet.next.next, pkt.tcp) or 
-                       eth_packet.next.next.dstport != rule['TCPPort']):
-                    continue
-                fm.match.tp_dst = rule['TCPPort']
-            
-            # Action handling
-            if rule.get('drop', False):
-                fm.actions = []
-            else:
-                out_port = table.get((dpid, eth_packet.dst))
-                if 'queue' in rule:
-                    fm.actions.append(of.ofp_action_enqueue(
-                        port=out_port, queue_id=rule['queue']))
+                    # Action handling
+                if rule['drop']:
+                    fm.actions = []
                 else:
-                    fm.actions.append(of.ofp_action_output(port=out_port))
-            
-            # Install rule and forward
-            event.connection.send(fm)
-            if not rule.get('drop', False):
-                msg = of.ofp_packet_out(data=event.ofp)
-                msg.actions.append(of.ofp_action_output(port=out_port))
-                event.connection.send(msg)
-            return
+                    out_port = table.get((dpid, eth_packet.dst))
+                    if out_port is None: continue
+                    
+                    if rule['queue'] != -1:
+                        fm.actions.append(of.ofp_action_enqueue(
+                            port=out_port, queue_id=rule['queue']))
+                    else:
+                        fm.actions.append(of.ofp_action_output(port=out_port))
+
+                # Install rule
+                event.connection.send(fm)
+                
+                # Forward packet if not drop
+                if not rule['drop']:
+                    msg = of.ofp_packet_out(data=event.ofp)
+                    msg.actions = fm.actions.copy()
+                    event.connection.send(msg)
+
+                break
+                #else ...
+                # => otherwise:
+                # => if the packet is an IP packet, its protocol is TCP, and the TCP port of the packet matches the TCP rule above
+                    # => add additioinal matching fileds to the flow rule you are creating: IP-protocol type, TCP_protocol_type, destination TCP port.
+                    # => like above if it requires QoS then use the of.ofp_action_enqueue() function
+                    # => also remember to check if this is a drop rule.
+                    # => at the end remember to send out both flow rule and packet
+
 
     ########### THIS IS THE END OF THE AREA WHERE YOU NEED TO ADD CODE ##################################
     #####################################################################################################
