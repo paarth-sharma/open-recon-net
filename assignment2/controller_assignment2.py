0bfc412fabb9436b60b29146936d1bee7cf21d1f
paarth-sharma <paarthsharma2003@gmail.com>
2025-02-25 23:16:54 +0000

    changed the queue assignments correctly in assignment2

--- assignment2/controller_assignment2.py
+++ assignment2/controller_assignment2.py
@@ -8,162 +8,163 @@ import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpidToStr
 from pox.lib.addresses import EthAddr
 
-
 log = core.getLogger()
 
-table={}
+# We store (dpid, MAC) -> port
+table = {}
 
-rules=[
-    # QoS Rules
-    {'EthSrc':'00:00:00:00:00:01', 'EthDst':'00:00:00:00:00:03', 'TCPPort':40, 'queue':0, 'drop':False},  # H1->H3: 30Mbps
-    {'EthSrc':'00:00:00:00:00:01', 'EthDst':'00:00:00:00:00:02', 'TCPPort':60, 'queue':1, 'drop':False},  # H1->H2: 150Mbps
+# Here we define *two* rules for each shaped flow: one for the shaped port,
+# one catch-all for “other traffic.” We also have drop rules for H3<->H4.
+rules = [
+  # 1) H1->H3 on TCP port 40 => shape at queue=0 (30Mb/s on s2-eth1),
+  {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:03','TCPPort':40,
+   'queue':0,'drop':False},
+  # 1b) H1->H3 catch-all => uncapped
+  {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:03',
+   'queue':0,'drop':False},
 
-    # H1->H4: uncapped => use queue=0 (very high max rate)
-    {'EthSrc':'00:00:00:00:00:01', 'EthDst':'00:00:00:00:00:04', 'queue':0, 'drop':False},
+  # 2) H1->H2 on TCP port 60 => shape at queue=1 (150Mb/s on s1-eth2)
+  {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:02','TCPPort':60,
+   'queue':1,'drop':False},
+  # 2b) H1->H2 catch-all => uncapped
+  {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:02',
+   'queue':0,'drop':False},
 
-    # H2->H1: uncapped => queue=0
-    {'EthSrc':'00:00:00:00:00:02','EthDst':'00:00:00:00:00:01','queue':0,'drop':False},
+  # 3) H1->H4 => uncapped
+  {'EthSrc':'00:00:00:00:00:01','EthDst':'00:00:00:00:00:04','queue':0,'drop':False},
 
-    # H2->H4: 200Mbps => queue=0 on s2-eth2
-    {'EthSrc':'00:00:00:00:00:02', 'EthDst':'00:00:00:00:00:04', 'queue':0, 'drop':False},
+  # 4) H2->H4 => shape at queue=1 (200Mb/s on s2-eth2)
+  {'EthSrc':'00:00:00:00:00:02','EthDst':'00:00:00:00:00:04','queue':1,'drop':False},
+  # 4b) Also allow H2->H1 => uncapped
+  {'EthSrc':'00:00:00:00:00:02','EthDst':'00:00:00:00:00:01','queue':0,'drop':False},
+  # 4c) Also allow H2->H3 => uncapped
+  {'EthSrc':'00:00:00:00:00:02','EthDst':'00:00:00:00:00:03','queue':0,'drop':False},
 
-    # H2->H3: uncapped => queue=0
-    {'EthSrc':'00:00:00:00:00:02', 'EthDst':'00:00:00:00:00:03', 'queue':0, 'drop':False},
+  # 5) H3->H1 => uncapped
+  {'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:01','queue':0,'drop':False},
+  # 5b) H3->H2 => uncapped
+  {'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:02','queue':0,'drop':False},
 
-    # H3->H1: uncapped => queue=0
-    {'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:01','queue':0,'drop':False},
-    # (duplicate kept per the template)
-    {'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:01','queue':0,'drop':False},
+  # 6) H4->H1 => uncapped
+  {'EthSrc':'00:00:00:00:00:04','EthDst':'00:00:00:00:00:01','queue':0,'drop':False},
+  # 6b) H4->H2 => uncapped
+  {'EthSrc':'00:00:00:00:00:04','EthDst':'00:00:00:00:00:02','queue':0,'drop':False},
+  # 6c) H4->H3 => blocked
+  #    (But we’ll do that with the firewall lines below)
 
-    # H4->H1: uncapped => queue=0
-    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:01', 'queue':0, 'drop':False},
-
-    # H4->H2: uncapped => queue=0
-    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:02', 'queue':0, 'drop':False},
-
-    # Firewall Rules - block H3 <--> H4
-    {'EthSrc':'00:00:00:00:00:03', 'EthDst':'00:00:00:00:00:04', 'drop':True},
-    {'EthSrc':'00:00:00:00:00:04', 'EthDst':'00:00:00:00:00:03', 'drop':True}
+  # Firewall rules: block H3 <-> H4
+  {'EthSrc':'00:00:00:00:00:03','EthDst':'00:00:00:00:00:04','drop':True},
+  {'EthSrc':'00:00:00:00:00:04','EthDst':'00:00:00:00:00:03','drop':True},
 ]
 
-
-def launch ():
+def launch():
     core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
-    core.openflow.addListenerByName("PacketIn",  _handle_PacketIn)
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
     log.info("Switch running.")
 
-
-def _handle_ConnectionUp ( event):
+def _handle_ConnectionUp(event):
     log.info("Starting Switch %s", dpidToStr(event.dpid))
-    # Remove any old flows
-    msg = of.ofp_flow_mod(command = of.OFPFC_DELETE)
+    # Clear old flows on switch
+    msg = of.ofp_flow_mod(command=of.OFPFC_DELETE)
     event.connection.send(msg)
 
-
-def _handle_PacketIn ( event):
-    dpid = event.connection.dpid
-    sw = dpidToStr(dpid)
-    inport = event.port
+def _handle_PacketIn(event):
+    dpid       = event.connection.dpid
+    sw         = dpidToStr(dpid)
+    inport     = event.port
     eth_packet = event.parsed
 
-    log.debug("Event: switch %s port %s packet %s" % (sw, inport, eth_packet))
+    log.debug("Event: switch %s port %s packet %s", sw, inport, eth_packet)
 
-    # Record which port we saw this SRC MAC on
+    # Learn the source MAC -> inport
     table[(dpid, eth_packet.src)] = inport
     dst_port = table.get((dpid, eth_packet.dst))
 
-    ######################################################################################
-    ############ CODE SHOULD ONLY BE ADDED BELOW  #################################
-
-    # ARP broadcast helper
-    if dst_port is None and eth_packet.type == eth.ARP_TYPE and \
-       eth_packet.dst == EthAddr(b"\xff\xff\xff\xff\xff\xff"):
-        # Flood ARP request so that hosts learn each other’s MAC
+    # 1) Handle ARP broadcast if we don't know the destination
+    if (dst_port is None and
+        eth_packet.type == eth.ARP_TYPE and
+        eth_packet.dst == EthAddr(b"\xff\xff\xff\xff\xff\xff")):
+        # Flood ARP request
         pkt_out = of.ofp_packet_out()
         pkt_out.in_port = inport
-        pkt_out.data    = event.ofp
-        pkt_out.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        pkt_out.data = event.ofp
+        pkt_out.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         event.connection.send(pkt_out)
         return
 
-    # Go through each rule in the dictionary
+    # 2) Check each firewall/QoS rule
     for rule in rules:
-        if (eth_packet.dst == EthAddr(rule['EthDst']) and eth_packet.src == EthAddr(rule['EthSrc'])):
-            log.debug("Event: found rule from %s -> %s" % (eth_packet.src, eth_packet.dst))
+        if (eth_packet.src == EthAddr(rule['EthSrc']) and
+            eth_packet.dst == EthAddr(rule['EthDst'])):
 
-            # If the rule has a TCPPort, ensure packet is TCP on that port
+            # If rule has 'TCPPort', verify packet is IPv4/TCP and matches that port
             if 'TCPPort' in rule:
                 if eth_packet.type != eth_packet.IP_TYPE:
                     continue
-                ip_packet = eth_packet.find('ipv4')
-                if not ip_packet or ip_packet.protocol != ip.TCP_PROTOCOL:
+                ip_pkt = eth_packet.find('ipv4')
+                if not ip_pkt or ip_pkt.protocol != ip.TCP_PROTOCOL:
                     continue
-                tcp_packet = eth_packet.find('tcp')
-                if not tcp_packet or tcp_packet.dstport != rule['TCPPort']:
+                tcp_pkt = eth_packet.find('tcp')
+                if not tcp_pkt or tcp_pkt.dstport != rule['TCPPort']:
                     continue
 
-            # Prepare a flow_mod with 40s soft_timeout
+            # We have a match => install a flow
             fm = of.ofp_flow_mod()
             fm.soft_timeout = 40
             fm.match.dl_src = eth_packet.src
             fm.match.dl_dst = eth_packet.dst
-
-            # If we matched a specific TCP port, add IP/TCP match fields
+            # If also matching TCP port
             if 'TCPPort' in rule:
                 fm.match.dl_type = 0x800  # IPv4
-                fm.match.nw_proto = 6    # TCP
+                fm.match.nw_proto = 6     # TCP
                 fm.match.tp_dst   = rule['TCPPort']
 
-            # If this rule is drop=True, install a drop flow
             if rule['drop']:
-                # no actions
+                # drop => no actions
                 pass
             else:
-                # If we found a known out port, enqueue or normal output
+                # otherwise forward out known port if we have it
                 if dst_port is not None:
                     if 'queue' in rule:
-                        fm.actions.append(of.ofp_action_enqueue(port = dst_port,
-                                                                queue_id = rule['queue']))
+                        fm.actions.append(of.ofp_action_enqueue(port=dst_port,
+                                                               queue_id=rule['queue']))
                     else:
-                        fm.actions.append(of.ofp_action_output(port = dst_port))
+                        fm.actions.append(of.ofp_action_output(port=dst_port))
 
-            # Send the flow_mod to the switch
+            # Send the flow_mod
             event.connection.send(fm)
 
-            # Also send this current packet out (packet_out)
+            # Also send the current packet
             po = of.ofp_packet_out()
-            po.data    = event.ofp
+            po.data = event.ofp
             po.in_port = inport
             if not rule['drop'] and dst_port is not None:
                 if 'queue' in rule:
-                    po.actions.append(of.ofp_action_enqueue(port = dst_port,
-                                                            queue_id = rule['queue']))
+                    po.actions.append(of.ofp_action_enqueue(port=dst_port,
+                                                           queue_id=rule['queue']))
                 else:
-                    po.actions.append(of.ofp_action_output(port = dst_port))
+                    po.actions.append(of.ofp_action_output(port=dst_port))
             event.connection.send(po)
 
-            # Done handling this packet
-            return
+            return  # done
 
-    # If we reach here, no rule matched => install a drop
+    # 3) If no rule matched => drop
     fm = of.ofp_flow_mod()
     fm.soft_timeout = 40
     fm.match.dl_src = eth_packet.src
     fm.match.dl_dst = eth_packet.dst
 
-    # If it’s IP/TCP, match those fields so that future identical flows also get dropped
+    # If IP, match that so future identical flows get dropped
     if eth_packet.type == eth_packet.IP_TYPE:
         fm.match.dl_type = 0x800
-        ip_packet = eth_packet.find('ipv4')
-        if ip_packet and ip_packet.protocol == ip.TCP_PROTOCOL:
-            fm.match.nw_proto = 6
-            tcp_packet = eth_packet.find('tcp')
-            if tcp_packet:
-                fm.match.tp_dst = tcp_packet.dstport
+        ip_pkt = eth_packet.find('ipv4')
+        if ip_pkt and ip_pkt.protocol == ip.TCP_PROTOCOL:
+            tcp_pkt = eth_packet.find('tcp')
+            if tcp_pkt:
+                fm.match.nw_proto = 6
+                fm.match.tp_dst   = tcp_pkt.dstport
 
-    # No actions => drop rule
     event.connection.send(fm)
-
-    # Also drop the *current* packet
+    # Also drop the current packet
     po = of.ofp_packet_out(data=event.ofp, in_port=inport)
     event.connection.send(po)
--- assignment2/topo_assignment2.py
+++ assignment2/topo_assignment2.py
@@ -6,121 +6,127 @@ import time
 import os
 
 def assignmentTopo():
-    net = Mininet( controller=RemoteController)
-    ######################################################################################
-    ############ CODE SHOULD ONLY BE ADDED BELOW  #################################
-    info( '*** Adding controller\n' )
-    # => add the controller here
-    net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+    # Create a two-switch topology
+    net = Mininet(controller=RemoteController)
 
-    info( '*** Adding hosts\n' )
-    h1 = net.addHost( 'h1', ip='10.0.0.1', mac='00:00:00:00:00:01' )
-    # => h1 is already added, now add the other hosts here
-    h2 = net.addHost( 'h2', ip='10.0.0.2', mac='00:00:00:00:00:02' )
-    h3 = net.addHost( 'h3', ip='10.0.0.3', mac='00:00:00:00:00:03' )
-    h4 = net.addHost( 'h4', ip='10.0.0.4', mac='00:00:00:00:00:04' )
+    info('*** Adding controller\n')
+    net.addController('c0', controller=RemoteController,
+                      ip='127.0.0.1', port=6633)
 
-    info( '*** Adding switches\n' )
-    # => add the switches here
-    s1 = net.addSwitch( 's1' )
-    s2 = net.addSwitch( 's2' )
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1', mac='00:00:00:00:00:01')
+    h2 = net.addHost('h2', ip='10.0.0.2', mac='00:00:00:00:00:02')
+    h3 = net.addHost('h3', ip='10.0.0.3', mac='00:00:00:00:00:03')
+    h4 = net.addHost('h4', ip='10.0.0.4', mac='00:00:00:00:00:04')
 
-    info( '*** Creating links\n' )
-    # => create the links here
-    net.addLink(h1, s1)   # s1-eth1
-    net.addLink(h2, s1)   # s1-eth2
-    net.addLink(s1, s2)   # s1-eth3, s2-eth3
-    net.addLink(h3, s2)   # s2-eth1
-    net.addLink(h4, s2)   # s2-eth2
+    info('*** Adding switches\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
 
-    info( '*** Starting network\n')
+    info('*** Creating links\n')
+    # h1 -> s1-eth1
+    net.addLink(h1, s1)
+    # h2 -> s1-eth2
+    net.addLink(h2, s1)
+    # s1 <-> s2 (assume s1-eth3 and s2-eth3)
+    net.addLink(s1, s2)
+    # h3 -> s2-eth1
+    net.addLink(h3, s2)
+    # h4 -> s2-eth2
+    net.addLink(h4, s2)
+
+    info('*** Starting network\n')
     net.start()
-    h1, h2, h3, h4 = net.hosts[0], net.hosts[1], net.hosts[2], net.hosts[3]
 
-    # => fill in here command to add two queues to the correct port of the correct switch:
-    # Q0 => 20..30 Mbps, Q1 => 50..150 Mbps for traffic from H1
-    os.system("sudo ovs-vsctl set port s1-eth1 qos=@newqos1 -- "
-              "--id=@newqos1 create qos type=linux-htb queues=0=@q0,1=@q1 -- "
-              "--id=@q0 create queue other-config:min-rate=20000000 other-config:max-rate=30000000 -- "
-              "--id=@q1 create queue other-config:min-rate=50000000 other-config:max-rate=150000000")
+    # For convenience
+    h1, h2, h3, h4 = net.hosts
+    # We want:
+    #  - 30Mb/s on s2-eth1 for H1->H3
+    #  - 150Mb/s on s1-eth2 for H1->H2
+    #  - 200Mb/s on s2-eth2 for H2->H4
+    #  - Everything else is "uncapped"
 
-    # => fill in here command to add one queue to s1-eth2 (for H2 -> somewhere):
-    # Q0 => 20..200 Mbps
-    os.system("sudo ovs-vsctl set port s1-eth2 qos=@newqos2 -- "
-              "--id=@newqos2 create qos type=linux-htb queues=0=@q0 -- "
-              "--id=@q0 create queue other-config:min-rate=20000000 other-config:max-rate=200000000")
+    # 1) s2-eth1 => queue0 = 30Mb/s
+    #    plus an uncapped queue1 if you want to handle H3->H1
+    os.system(
+        "ovs-vsctl set port s2-eth1 qos=@newqos1 -- "
+        "--id=@newqos1 create qos type=linux-htb queues=0=@q0,1=@q1 -- "
+        "--id=@q0 create queue other-config:min-rate=20000000 other-config:max-rate=30000000 -- "
+        "--id=@q1 create queue other-config:min-rate=20000000 other-config:max-rate=1000000000000"
+    )
 
-    # => fill in here command to add two queues to s2-eth2 (for H4), so you can shape H2->H4 at 200Mb if you like
-    os.system("sudo ovs-vsctl set port s2-eth2 qos=@newqos3 -- "
-              "--id=@newqos3 create qos type=linux-htb queues=0=@q0,1=@q1 -- "
-              "--id=@q0 create queue other-config:min-rate=20000000 other-config:max-rate=200000000 -- "
-              "--id=@q1 create queue other-config:min-rate=50000000 other-config:max-rate=1000000000000")
+    # 2) s1-eth2 => queue0 = uncapped, queue1 = 150Mb/s
+    os.system(
+        "ovs-vsctl set port s1-eth2 qos=@newqos2 -- "
+        "--id=@newqos2 create qos type=linux-htb queues=0=@q0,1=@q1 -- "
+        "--id=@q0 create queue other-config:min-rate=20000000 other-config:max-rate=1000000000000 -- "
+        "--id=@q1 create queue other-config:min-rate=50000000 other-config:max-rate=150000000"
+    )
 
+    # 3) s2-eth2 => queue0 = uncapped, queue1 = 200Mb/s
+    os.system(
+        "ovs-vsctl set port s2-eth2 qos=@newqos3 -- "
+        "--id=@newqos3 create qos type=linux-htb queues=0=@q0,1=@q1 -- "
+        "--id=@q0 create queue other-config:min-rate=20000000 other-config:max-rate=1000000000000 -- "
+        "--id=@q1 create queue other-config:min-rate=50000000 other-config:max-rate=200000000"
+    )
 
-    ########### THIS IS THE END OF THE AREA WHERE YOU NEED TO ADD CODE ##################################
-    #####################################################################################################
-    # The lines below run tests automatically:
+    # Reduce TCP retries for quicker iperf tests
+    for h in (h1, h2, h3, h4):
+        h.cmd('sysctl -w net.ipv4.tcp_syn_retries=1')
+        h.cmd('sysctl -w net.ipv4.tcp_retries=1')
 
-    # Reduce TCP retries to speed up testing
-    h1.cmd('sudo sysctl -w net.ipv4.tcp_syn_retries=1')
-    h1.cmd('sudo sysctl -w net.ipv4.tcp_retries=1')
-    h2.cmd('sudo sysctl -w net.ipv4.tcp_syn_retries=1')
-    h2.cmd('sudo sysctl -w net.ipv4.tcp_retries=1')
-    h3.cmd('sudo sysctl -w net.ipv4.tcp_syn_retries=1')
-    h3.cmd('sudo sysctl -w net.ipv4.tcp_retries=1')
-    h4.cmd('sudo sysctl -w net.ipv4.tcp_syn_retries=1')
-    h4.cmd('sudo sysctl -w net.ipv4.tcp_retries=1')
-
-    info( '\n\n\n\n*** Testing CIR from H1 to H3 port 40 - should be capped at 30Mb/s\n')
+    # Now run the assignment tests
+    info('\n*** Testing CIR from H1 to H3 port 40 - should be ~30Mb/s\n')
     h3.cmd('iperf -s -p40 &')
     print(h1.cmd('iperf -c %s -p40' % h3.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing CIR from H1 to H2 port 60 - should be capped at 150Mb/s\n')
+    info('\n*** Testing CIR from H1 to H2 port 60 - should be ~150Mb/s\n')
     h2.cmd('iperf -s -p60 &')
     print(h1.cmd('iperf -c %s -p60' % h2.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing CIR from H1 to H4 - should not be capped\n')
+    info('\n*** Testing CIR from H1 to H4 - should not be capped\n')
     h4.cmd('iperf -s &')
     print(h1.cmd('iperf -c %s' % h4.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing CIR from H2 to H4 - should be capped at 200Mb/s\n')
+    info('\n*** Testing CIR from H2 to H4 - should be ~200Mb/s\n')
     print(h2.cmd('iperf -c %s' % h4.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing CIR from H4 to H1 - should not be capped\n')
+    info('\n*** Testing CIR from H4 to H1 - should not be capped\n')
     h1.cmd('iperf -s &')
     print(h4.cmd('iperf -c %s' % h1.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing link from H3 to H4 - should be blocked\n')
+    info('\n*** Testing link from H3 to H4 - should be blocked\n')
     print(h3.cmd('iperf -c %s' % h4.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing link from H4 to H2 - should be uncapped\n')
+    info('\n*** Testing link from H4 to H2 - should be uncapped\n')
     print(h4.cmd('iperf -c %s -p60' % h2.IP()))
     time.sleep(3)
 
-    info( '\n\n\n\n*** Testing link from H4 to H3 - should be blocked\n')
+    info('\n*** Testing link from H4 to H3 - should be blocked\n')
     print(h4.cmd('iperf -c %s -p40' % h3.IP()))
     time.sleep(1)
 
-    CLI( net )
+    CLI(net)
 
     # Cleanup QoS
-    os.system('sudo ovs-vsctl clear Port s1-eth1 qos')
-    os.system('sudo ovs-vsctl clear Port s1-eth2 qos')
-    os.system('sudo ovs-vsctl clear Port s1-eth3 qos')
-    os.system('sudo ovs-vsctl clear Port s2-eth1 qos')
-    os.system('sudo ovs-vsctl clear Port s2-eth2 qos')
-    os.system('sudo ovs-vsctl clear Port s2-eth3 qos')
-    os.system('sudo ovs-vsctl --all destroy qos')
-    os.system('sudo ovs-vsctl --all destroy queue')
+    os.system('ovs-vsctl clear Port s1-eth1 qos')
+    os.system('ovs-vsctl clear Port s1-eth2 qos')
+    os.system('ovs-vsctl clear Port s1-eth3 qos')
+    os.system('ovs-vsctl clear Port s2-eth1 qos')
+    os.system('ovs-vsctl clear Port s2-eth2 qos')
+    os.system('ovs-vsctl clear Port s2-eth3 qos')
+    os.system('ovs-vsctl --all destroy qos')
+    os.system('ovs-vsctl --all destroy queue')
 
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
+    setLogLevel('info')
     assignmentTopo()
